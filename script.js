ef2399228a70a4393340ffe9ab310e817a003511
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2024-12-16 23:07:34 -0300

    javascript cards e liivro

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
-const myObserve = new IntersectionObserver((entries) => {
+
+//section Livro
+
+const myLivro = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if(entry.isIntersecting){
-      entry.target.classList.add('show')
-      document.querySelector('.anime-1').classList.add('animacao-card-1')
-      document.querySelector('.anime-3').classList.add('animacao-card-3')
+
+      document.querySelector('.off-amarelo').classList.add('amarelo')
+      document.querySelector('.off-roxo').classList.add('roxo')
+      document.querySelector('.off-lata').classList.add('lata')
+      document.querySelector('.off-seta-capa').classList.add('seta-capa')
+      document.querySelector('.off-seta-broche').classList.add('seta-broche')
+      document.querySelector('.off-seta-marca').classList.add('seta-marca')
+      
     }else {
-      entry.target.classList.remove('show')
-      document.querySelector('.anime-1').classList.remove('animacao-card-1')
-      document.querySelector('.anime-3').classList.remove('animacao-card-3')
+
+      document.querySelector('.off-amarelo').classList.remove('amarelo')
+      document.querySelector('.off-roxo').classList.remove('roxo')
+      document.querySelector('.off-lata').classList.remove('lata')
+      document.querySelector('.off-seta-capa').classList.remove('seta-capa')
+      document.querySelector('.off-seta-broche').classList.remove('seta-broche')
+      document.querySelector('.off-seta-marca').classList.remove('seta-marca')
+
     }
   })
 })
 
-const elements = document.querySelectorAll('.hidden')
-elements.forEach((element) => myObserve.observe(element))
+const sectionlivro = document.querySelectorAll('.livro-java')
+sectionlivro.forEach((element) => myLivro.observe(element))
+
+
+//Section cards
+
+const myCartas = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+
+      entry.target.classList.add('show')
+      document.querySelector('.anime-1').classList.add('animacao-card-1')
+      document.querySelector('.anime-3').classList.add('animacao-card-3')
+
+    }else {
+
+      entry.target.classList.remove('show')
+      document.querySelector('.anime-1').classList.remove('animacao-card-1')
+      document.querySelector('.anime-3').classList.remove('animacao-card-3')
+
+    }
+  })
+})
+
+const sectioncard = document.querySelectorAll('.hidden')
+sectioncard.forEach((element) => myCartas.observe(element))
